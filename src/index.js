d800976e2a23e598953097e7958e5bad9c273440
RAKESH ROSHAN <rockieroshan@gmail.com>
2017-12-27 18:30:34 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Blog from './containers/Blog/Blog';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Blog />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
